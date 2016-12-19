a8b88c04ea0a6b3f355dad1472b5b940989d9ada
yongpark <yongpark.sd@gmail.com>
2016-12-18 20:32:49 -0800

    adds w5d4

---
+++ W5/D2/skeleton/lib/controller_base.rb
@@ -0,0 +1,56 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'erb'
+require_relative './session'
+
+class ControllerBase
+  attr_reader :req, :res, :params
+
+  # Setup the controller
+  def initialize(req, res)
+    @ereq = req
+    @res = res
+  end
+
+  # Helper method to alias @already_built_response
+  def already_built_response?
+    @already_built_response ||= false
+  end
+
+  # Set the response status code and header
+  def redirect_to(url)
+    raise if already_built_response?
+    @res.status = 302
+    @res["Location"] = url
+    @already_built_response = true
+  end
+
+  # Populate the response with content.
+  # Set the response's content type to the given type.
+  # Raise an error if the developer tries to double render.
+  def render_content(content, content_type)
+  raise if already_built_response?
+   @res.write(content)
+   @res['Content-Type'] = content_type
+   @already_built_response = true
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name)
+    controller_name = self.class.to_s.underscore
+    template_path = "views/#{controller_name}/#{template_name}.html.erb"
+    string = File.read(template_path)
+    erb_temp ERB.new(string).result(binding)
+    render_content(erb_temp, 'text/html')
+  end
+
+  # method exposing a `Session` object
+  def session
+    @session ||= Session.new(@req)
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+  end
+end
