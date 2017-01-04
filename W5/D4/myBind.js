a0e362931622d4695fd5cea350de36ee826ccf73
yongpark <yongpark.sd@gmail.com>
2017-01-04 15:49:44 -0800

    fixes missing files in w5

---
+++ W5/D4/myBind.js
@@ -0,0 +1,35 @@
+class Lamp {
+  constructor() {
+    this.name = "a lamp";
+  }
+}
+//
+const turnOn = function() {
+   console.log("Turning on " + this.name);
+};
+
+// const lamp = new Lamp();
+//
+// turnOn(); // should not work the way we want it to
+//
+// const boundTurnOn = turnOn.bind(lamp);
+// const myBoundTurnOn = turnOn.myBind(lamp);
+//
+// boundTurnOn(); // should say "Turning on a lamp"
+// myBoundTurnOn(); // should say "Turning on a lamp"
+
+const lamp = new Lamp();
+
+turnOn();
+
+const boundTurnOn = turnOn.bind(lamp);
+boundTurnOn();
+
+Function.prototype.myBind = function(context){
+  return (() => this.apply(context));
+};
+
+turnOn.apply(lamp);
+
+const myBoundTurnOn = turnOn.myBind(lamp);
+myBoundTurnOn();
---
+++ W5/D5/Asteroids/lib/game.js
@@ -0,0 +1,6 @@
+const Util = require('./utils.js');
+const Bullet = require('./bullet.js');
+const Ship = require("./ship");
+const Asteroid = require('./asteroid.js');
+
+function Game (){}
